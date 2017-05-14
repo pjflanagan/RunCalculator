3a3eb4f06c40a8e610e9dd3ebd2305d21fc79132
pjflanagan <pjflanagan1@gmail.com>
2017-05-14 13:31:10 -0400

    enter your own time

--- script.js
+++ script.js
@@ -14,12 +14,14 @@ var split_distances = [200, 400, 800, 1000, convert(1)];
 var total_seconds = 0,
 	race = 7,
 	split = 4,
-	pacemode = false;
+	pacemode = false,
+	distmode = false,
+	unit = false; //true for kilos false for miles
 
 selectEvent(2);
 selectSplit(split);
 
-function convert(x){ 
+function convert(x){
 	//miles to meters
 	return x*1609.344;
 }
@@ -60,7 +62,12 @@ function output(){
 	hourmode = false;
 	//random();
 	//setTimeout(function(){
-	factor = (!pacemode) ? split_distances[split] / event_distances[race] : event_distances[race] / split_distances[split] ;
+	if(!distmode)
+		factor = (!pacemode) ? split_distances[split] / event_distances[race] : event_distances[race] / split_distances[split];
+	else {
+		distance = (unit) ? document.getElementById("dist-num").value * 1000 : convert(document.getElementById("dist-num").value);
+		factor = (!pacemode) ? split_distances[split] / distance : distance / split_distances[split];
+	}
 	if(factor > 1 & !pacemode) return cancel();
 
 	split_time = total_seconds * factor;
@@ -81,7 +88,7 @@ function output(){
 
 		rm1 = Math.floor(split_time/60/60%10);
 	}
-	
+
 	document.getElementById("rm10").innerHTML = rm10;
 	document.getElementById("rm1").innerHTML = rm1;
 	document.getElementById("rs10").innerHTML = rs10;
@@ -154,9 +161,44 @@ function togglePaceMode(){
 	input();
 }
 
-function toggleManualDistance(){
-	//pacemode = !pacemode;
-	//if(pacemode)document.getElementById("item-2-title").innerHTML = "DIST";
-	//else document.getElementById("item-2-title").innerHTML = "EVENT";
-	//input();
+function toggleUnit(){
+	unit = !unit;
+	if(distmode){
+		if(unit) document.getElementById("dist-unit-td").innerHTML = "KILOS";
+		else document.getElementById("dist-unit-td").innerHTML = "MILES";
+	}
+	input();
+}
+
+function toggleEventMode(){
+	distmode = !distmode;
+	//should have both written in index.html and change from hidden to visible
+	if(distmode){
+		document.getElementById("item-1-title").innerHTML = "DIST";
+		document.getElementById("item-1-input").innerHTML = "" +
+			"<table id=\"dist-table\">" +
+			"<tr>" +
+			"<td class=\"gap\"></td>" +
+			"<td id=\"dist-input-td\"><input type=\"number\" id=\"dist-num\" onkeypress=\"input()\" value=\"00.00\"/></td>" +
+			"<td id=\"dist-unit-td\" onclick=\"toggleUnit();\" ontouchstart=\"event.preventDefault();toggleUnit();\">MILES</td>" +
+			"<td class=\"gap\"></td>" +
+			"</tr>" +
+			"</table>";
+	}
+	else {
+		document.getElementById("item-1-title").innerHTML = "EVENT";
+		document.getElementById("item-1-input").innerHTML = "" +
+			"<table id=\"event-table\" ontouchstart>" +
+			"<tr>" +
+			"<td class=\"end hover\" id=\"event0\" onclick=\"selectEvent(0);\"></td>" +
+			"<td class=\"mid hover\" id=\"event1\" onclick=\"selectEvent(1);\"></td>" +
+			"<td class=\"selected-split\" id=\"event2\">5K</td>" +
+			"<td class=\"mid hover\" id=\"event3\" onclick=\"selectEvent(3);\"></td>" +
+			"<td class=\"end hover\" id=\"event4\" onclick=\"selectEvent(4);\"></td>" +
+			"</tr>" +
+			"</table>";
+			selectEvent(2);
+			selectSplit(split);
+	}
+	input();
 }
